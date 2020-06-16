ecc7249928499bacd746d9b43520242c2f292548
unnnyong <unnnyong@gmail.com>
2020-06-17 01:03:45 +0900

    Add Xcode Project file

---
+++ LottoNumberMaker/LottoNumberMaker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottoNumberMaker
+//
+//  Created by Eunyeong Kim on 2020/06/17.
+//  Copyright © 2020 Eunyeong Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
